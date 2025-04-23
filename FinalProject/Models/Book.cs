1ed1e116d4838f9d99642a0ad3e0d7ea415bbf59
jess <jessielinsangan@gmail.com>
2025-04-23 16:39:22 -0500

    Initialize in-memory database & create base objects

---
+++ FinalProject/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalProject.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+
+        public Author? Author { get; set; }
+    }
+}
