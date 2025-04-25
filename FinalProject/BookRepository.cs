c74b09978e275b7809171159ff4ba681a4080bb7
bee <abeebuzzy.247@gmail.com>
2025-04-25 09:50:33 -0500

    add methods getbooks and getauthors memory-cache

---
+++ FinalProject/BookRepository.cs
@@ -0,0 +1,13 @@
+﻿using FinalProject.Models;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace FinalProject
+{
+    internal class BookRepository
+    {
+        //internal Task<List<Book>?> GetBooks()
+        //{
+        //    //
+        //}
+    }
+}
