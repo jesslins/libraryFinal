6b6796728f1a251c4c0350d2424531aa3c991132
jess <jessielinsangan@gmail.com>
2025-04-27 14:25:45 -0500

    Link endpoints to database using newly created DTOs

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,4 +1,3 @@
-
 using FinalProject.Models;
 using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
@@ -24,7 +23,7 @@ namespace FinalProject
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-	    builder.Services.AddMemoryCache();
+            builder.Services.AddMemoryCache();
             builder.Services.AddRateLimiter(_ => _
                 .AddFixedWindowLimiter(policyName: "fixed", options =>
                 {
@@ -34,73 +33,99 @@ namespace FinalProject
 
             var app = builder.Build();
 
-            var authors = new List<Author>();
-            var books = new List<Book>();
-
             // 1st GET all authours
-            app.MapGet("/authors", () => authors);
+            app.MapGet("/authors", (LibraryContext db) =>
+                 db.Authors
+                   .Include(a => a.Books)
+                   .Select(a => new AuthorDto
+                   {
+                       Id = a.Id,
+                       Name = a.Name,
+                       Books = a.Books.Select(b => new BookDto
+                       {
+                           Id = b.Id,
+                           Title = b.Title
+                       }).ToList()
+                   }).ToList());
+
 
             // 2nd GET all books
-            app.MapGet("/books", () => books);
+            app.MapGet("/books", (LibraryContext db) =>
+               db.Books
+                 .Include(b => b.Author)
+                 .Select(b => new
+                 {
+                     Id = b.Id,
+                     Title = b.Title,
+                     AuthorName = b.Author.Name
+                 }).ToList());
 
             // 1 POST create new authour
-            app.MapPost("/authors", (Author author) =>
+            app.MapPost("/authors", (LibraryContext db, Author author) =>
             {
-                authors.Add(author);
+                db.Authors.Add(author);
+                db.SaveChanges();
                 return Results.Created($"/authors/{author.Id}", author);
             });
 
             // 2 POST create new book
-            app.MapPost("/books", (Book book) =>
+            app.MapPost("/books", (LibraryContext db, Book book) =>
             {
-                books.Add(book);
-                var author = authors.FirstOrDefault(a => a.Id == book.AuthorId);
-                if (author is not null)
+                db.Books.Add(book);
+                db.SaveChanges();
+                var author = db.Authors.FirstOrDefault(a => a.Id == book.AuthorId);
+                if (author is null)
                 {
-                    author.Books.Add(book); // add to the corresponding authour list
+                    return Results.NotFound($"Author with ID {book.AuthorId} not found.");
                 }
 
-                return Results.Created($"/books/{book.Id}", book);
+                author.Books.Add(book);
+                db.SaveChanges();
+                return Results.Created($"/books/{book.Id}", new { book.Id, book.Title, AuthorId = book.AuthorId});
             });
 
-            // 1 PUT completely update an authour
-            app.MapPut("/authors/{id}", (int id, Author updatedAuthor) =>
+            // 1 PUT completely update an author
+            app.MapPut("/authors/{id}", (LibraryContext db, int id, Author updatedAuthor) =>
             {
-                var author = authors.FirstOrDefault(a => a.Id == id);
+                var author = db.Authors.FirstOrDefault(a => a.Id == id);
                 if (author is null) return Results.NotFound();
 
                 author.Name = updatedAuthor.Name;
-                return Results.Ok(author);
+                db.SaveChanges();
+                return Results.Ok(new {author.Id, author.Name});
             });
 
-            // 2 PUT completely update an anuthor
-            app.MapPut("/books/{id}", (int id, Book updatedBook) =>
+            // 2 PUT completely update a book
+            app.MapPut("/books/{id}", (LibraryContext db, int id, Book updatedBook) =>
             {
-                var book = books.FirstOrDefault(b => b.Id == id);
+                var book = db.Books.FirstOrDefault(b => b.Id == id);
                 if (book is null) return Results.NotFound();
 
                 book.Title = updatedBook.Title;
                 book.AuthorId = updatedBook.AuthorId;
-                return Results.Ok(book);
+                db.SaveChanges();
+                return Results.Ok(new { book.Id, book.Title, AuthorId = book.AuthorId });
             });
 
-            // 1 DELETE authuor
-            app.MapDelete("/authors/{id}", (int id) =>
+            // 1 DELETE author
+            app.MapDelete("/authors/{id}", (LibraryContext db, int id) =>
             {
-                var author = authors.FirstOrDefault(a => a.Id == id);
+                var author = db.Authors.FirstOrDefault(a => a.Id == id);
                 if (author is null) return Results.NotFound();
 
-                authors.Remove(author);
+                db.Authors.Remove(author);
+                db.SaveChanges();
                 return Results.Ok();
             });
 
             // 2 DELETE book
-            app.MapDelete("/books/{id}", (int id) =>
+            app.MapDelete("/books/{id}", (LibraryContext db, int id) =>
             {
-                var book = books.FirstOrDefault(b => b.Id == id);
+                var book = db.Books.FirstOrDefault(b => b.Id == id);
                 if (book is null) return Results.NotFound();
 
-                books.Remove(book);
+                db.Books.Remove(book);
+                db.SaveChanges();
                 return Results.Ok();
             });
 
@@ -119,11 +144,46 @@ namespace FinalProject
             //app.MapGet("/authors/memory-cache", GetAuthorsWithMemoryCache);
 
             app.UseHttpsRedirection();
+            app.UseRateLimiter();
 
             app.UseAuthorization();
 
 
             app.MapControllers();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+
+                var authors = new List<Author>
+                {
+                    new Author { Name = "J.K. Rowling" },
+                    new Author { Name = "J.R.R. Tolkien" },
+                    new Author { Name = "George R.R. Martin" },
+                    new Author { Name = "Agatha Christie" },
+                    new Author { Name = "Isaac Asimov" }
+                };
+
+                db.Authors.AddRange(authors);
+                db.SaveChanges();
+
+                var books = new List<Book>
+                {
+                    new Book { Title = "Harry Potter and the Sorcerer's Stone", AuthorId = authors[0].Id },
+                    new Book { Title = "Harry Potter and the Chamber of Secrets", AuthorId = authors[0].Id },
+                    new Book { Title = "The Hobbit", AuthorId = authors[1].Id },
+                    new Book { Title = "The Lord of the Rings", AuthorId = authors[1].Id },
+                    new Book { Title = "A Game of Thrones", AuthorId = authors[2].Id },
+                    new Book { Title = "A Clash of Kings", AuthorId = authors[2].Id },
+                    new Book { Title = "Murder on the Orient Express", AuthorId = authors[3].Id },
+                    new Book { Title = "And Then There Were None", AuthorId = authors[3].Id },
+                    new Book { Title = "Foundation", AuthorId = authors[4].Id },
+                    new Book { Title = "I, Robot", AuthorId = authors[4].Id }
+                };
+
+                db.Books.AddRange(books);
+                db.SaveChanges();
+            }
 
             app.Run();
         }
@@ -176,5 +236,4 @@ namespace FinalProject
     //    });
     //}
 
-    }
-
+}
